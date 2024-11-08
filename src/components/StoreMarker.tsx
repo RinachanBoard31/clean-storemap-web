3f11227becbc08534e239b7fa7fa1b4b2a6eba64
tanakenology <tanakenology@gmail.com>
2024-11-08 22:32:42 +0900

    opening-hours: ユーザーは半径 500 m 以内のレストランの開閉店時間一覧を見れる

--- src/components/StoreMarker.tsx
+++ src/components/StoreMarker.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import { AdvancedMarker, InfoWindow, useMap, useAdvancedMarkerRef } from '@vis.gl/react-google-maps';
 import styles from './StoreMarker.module.scss';
+import DollarIcon from '../assets/dollar.svg';
 
 type Props = {
   store: { id: string; name: string; regularOpeningHours: string; priceLevel: string; location: { latitude: string; longitude: string } };
@@ -21,6 +22,26 @@ export const StoreMarker: React.FC<Props> = (props) => {
     props.onMarkerClick();
   }, [map, props.onMarkerClick]);
   const handleClose = useCallback(() => setInfoWindowShown(false), []);
+  const dollarIcon = <img src={DollarIcon} alt="DollarIcon" />;
+
+  function priceLevelToText(priceLevel: string) {
+    switch (priceLevel) {
+      case "PRICE_LEVEL_UNSPECIFIED":
+        return (<p>価格指定なし</p>);
+      case "PRICE_LEVEL_FREE":
+        return (<p>無料</p>);
+      case "PRICE_LEVEL_INEXPENSIVE":
+        return (dollarIcon);
+      case "PRICE_LEVEL_MODERATE":
+        return (<>{dollarIcon}{dollarIcon}</>);
+      case "PRICE_LEVEL_EXPENSIVE":
+        return (<>{dollarIcon}{dollarIcon}{dollarIcon}</>);
+      case "PRICE_LEVEL_VERY_EXPENSIVE":
+          return (<>{dollarIcon}{dollarIcon}{dollarIcon}{dollarIcon}</>);
+      default:
+        return (<p>価格指定なし</p>);
+    }
+  }
 
   useEffect(() => {
     setInfoWindowShown(props.isActive);
@@ -37,10 +58,10 @@ export const StoreMarker: React.FC<Props> = (props) => {
       {infoWindowShown && (
         <InfoWindow className={styles.body} anchor={marker} onClose={handleClose}>
           <h2>{props.store.name}</h2>
-          <h3>{props.store.priceLevel}</h3>
+          {priceLevelToText(props.store.priceLevel)}
           <ul className={styles.list} >
-            {props.store.regularOpeningHours.split(", ").map((regularOpeningHour) => (
-              <li>{regularOpeningHour}</li>
+            {props.store.regularOpeningHours.split(", ").map((regularOpeningHour, index) => (
+              <li key={props.store.id + regularOpeningHour + index}>{regularOpeningHour}</li>
             ))}
           </ul>
         </InfoWindow>
