c7b3b3833d8e8954045d22828ce2a4ed966704af
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2024-11-22 23:41:25 +0900

    クッキーの削除と遷移を同時に行う際に遷移が先に行われ正しい遷移先にいかない問題の修正

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import { Login } from "./components/Login";
 import { useSession } from "./hooks/sessionUser";
 
 function App() {
-  const { deleteSession, isAuthenticated } = useSession();
+  const { deleteSession, isAuthenticated, getSessionId } = useSession();
   const navigate = useNavigate(); // 画面遷移をするためにuseNavigate フックを使用
 
   const location = useLocation(); // URLのpathを取得する
@@ -38,7 +38,7 @@ function App() {
     ) {
       navigate("/");
     }
-  }, [navigate]); // ページが変わったときに実行
+  }, [getSessionId(), navigate]); // クッキーを削除したと気に、削除前に画面遷移の処理が走ってしまうので、クッキーの削除を監視して削除する
 
   function handleLogout() {
     deleteSession();
