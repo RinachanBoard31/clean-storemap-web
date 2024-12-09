e443197dad07ad2996c5f8f4332441c3a883c60c
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2024-12-09 17:59:47 +0900

    エラーの表示

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -2,8 +2,11 @@ import { AppTitle } from "../AppTitle";
 import { EarthVideo } from "../EarthVideo"; // 動画ファイルをインポート
 import { useLogin } from "../../hooks/auth/useLogin";
 import "../../css/auth/auth.css";
+import { ErrorMessage } from "../common/ErrorMessage.tsx";
 
 export const Login = () => {
+  const query = new URLSearchParams(window.location.search);
+  const error = query.get("error") ? "登録してください。" : "";
   function handleLogin() {
     useLogin();
   }
@@ -13,6 +16,11 @@ export const Login = () => {
       <div className="contents-area">
         {EarthVideo()}
         <div className="action-contents">
+          {error && (
+            <div className="error">
+              <ErrorMessage message={error} />
+            </div>
+          )}
           <button onClick={handleLogin}>ログイン</button>
           <br />
           <a href="signup">新規登録の方はこちら</a>
